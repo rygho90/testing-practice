9d5d803730eccbc4155b5edebcab87fb1a33d02d
Ryan <rygho90@gmail.com>
2022-07-07 19:44:06 -0400

    Add tests for caesarCipher function

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,20 +1,16 @@
 function caesarCipher(str) {
-    const letterIndex = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G':7,
-                         'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14,
-                         'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21,
-                         'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
-
-    const rLetterIndex = { 1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G',
-                         8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'L', 13: 'M', 14: 'N',
-                         15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T', 21: 'U',
-                         22: 'V', 23: 'W', 24: 'X', 25: 'Y', 26: 'Z'}
-
     let newStr = ''
     for (let i = 0; i < str.length; i++) {
-        if (str[i].toUpperCase() == rLetterIndex[26]) {
-            newStr += 'A'
+        if (str[i] === ' ') {
+            newStr += str[i]
         } else {
-            newStr += rLetterIndex[letterIndex[str[i]] + 1]
+            if (str.charCodeAt(i) === 122 || str.charCodeAt(i) === 90) {
+                let newCode = str.charCodeAt(i) - 25
+                newStr += String.fromCharCode(newCode)
+            } else {
+                let newCode = str.charCodeAt(i) + 1;
+                newStr += String.fromCharCode(newCode)
+            }
         }
     }
     return newStr
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -3,3 +3,19 @@ import caesarCipher from './caesarCipher'
 test('encrypt an all upper case string', () => {
     expect(caesarCipher('HELLO')).toBe('IFMMP')
 })
+
+test('encrypt an all lower case string', () => {
+    expect(caesarCipher('hello')).toBe('ifmmp')
+})
+
+test('encrypt a mixed case string', () => {
+    expect(caesarCipher('hElLo')).toBe('iFmMp')
+})
+
+test('encrypt a string with a space', () => {
+    expect(caesarCipher('hello world')).toBe('ifmmp xpsme')
+})
+
+test('encrypt a string which includes a z', () => {
+    expect(caesarCipher('Zzzzzz')).toBe('Aaaaaa')
+})
